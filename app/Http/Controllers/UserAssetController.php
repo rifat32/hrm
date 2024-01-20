9c264c34e23b84879a5b8171a4ba5befd4c1a2d0
rifat32 <rifatbilalphilips@gmail.com>
2024-01-21 02:25:30 +0600

    update

--- app/Http/Controllers/UserAssetController.php
+++ app/Http/Controllers/UserAssetController.php
@@ -665,10 +665,10 @@ class UserAssetController extends Controller
                 //       return $query->where('user_assets.user_id', $request->user()->id);
                 //   })
                   ->when(!empty($request->start_date), function ($query) use ($request) {
-                      return $query->where('user_assets.created_at', ">=", $request->start_date);
+                      return $query->where('user_assets.date', ">=", $request->start_date);
                   })
                   ->when(!empty($request->end_date), function ($query) use ($request) {
-                      return $query->where('user_assets.created_at', "<=", ($request->end_date . ' 23:59:59'));
+                      return $query->where('user_assets.date', "<=", ($request->end_date . ' 23:59:59'));
                   })
                   ->when(!empty($request->order_by) && in_array(strtoupper($request->order_by), ['ASC', 'DESC']), function ($query) use ($request) {
                       return $query->orderBy("user_assets.id", $request->order_by);
